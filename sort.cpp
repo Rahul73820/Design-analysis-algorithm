8079b6dd184de6fb0f7e1b6933de9fb9f94cad19
Rahul73820 <113227945+Rahul73820@users.noreply.github.com>
2023-09-03 09:39:41 +0530

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],i,j,n,swap;
+	printf("enter range:");
+	scanf("%d",n);
+	printf("enter the array elements:");
+	for(i=0;i<n;i++)
+	{
+	for (j=i+1;j<n;j++)
+	{
+		a[i]=swap;
+		swap=a[j];
+	     }     
+	 return 0;
+	     
+
+	}
+}
